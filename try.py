07132d7753bab4351da09063864a034ba46ea8e5
GiannnisTz <45824529+GiannnisTz@users.noreply.github.com>
2020-04-08 16:02:20 +0300

    Add files via upload

--- try.py
+++ try.py
@@ -1,8 +1,5 @@
 import sys
-from functools import reduce
-
 import pandas as pd
-import numpy as np
 
 print('Python version ' + sys.version)
 print('Pandas version ' + pd.__version__)
@@ -14,23 +11,6 @@ Location3 = r'C:\Users\pltzi\Desktop\data mining\data\march\listings.csv'
 # filelist1 = os.listdir(Location1) # April
 # filelist2 = os.listdir(Location2) # February
 # filelist3 = os.listdir(Location3) # March
-#
-#
-# print(filelist1)
-# print(filelist2)
-# print(filelist3)
-
-# df_list = []
-# i = 0
-#
-# for file in filelist:
-#     fin = os.path.join(Location1,filelist[i])
-#     df = pd.read_csv(fin,low_memory=False)
-#     df_list.append(df)
-#     i = i + 1
-#
-# big_df = pd.concat(df_list)
-# print(big_df)
 
 df1 = pd.read_csv(Location1, usecols=[
     'id',
@@ -65,7 +45,6 @@ df1 = pd.read_csv(Location1, usecols=[
     'minimum_nights',
     'room_type'
 ])
-
 df2 = pd.read_csv(Location2, usecols=[
     'id',
     'zipcode',
@@ -98,7 +77,6 @@ df2 = pd.read_csv(Location2, usecols=[
     'availability_365',
     'minimum_nights'
 ])
-
 df3 = pd.read_csv(Location3, usecols=[
     'id',
     'zipcode',
@@ -132,35 +110,21 @@ df3 = pd.read_csv(Location3, usecols=[
     'minimum_nights'
 ])
 
-df = df1
-
-# MERGE
-# df = pd.merge(df1, df2, on=['id', 'name', 'latitude', 'longitude'], how="outer")
-# df = pd.merge(df, df3, on=['id', 'name', 'latitude', 'longitude'], how="outer")
-
-# Combine first solution
-
-# df.combine_first(df2)
-# df.combine_first(df3)
-
-dfs=[df1,df2,df3]
-df4=reduce(lambda left,right: pd.merge(left,right,on='id'),dfs)
-
 # # method for finding all missing data
 # bool_series = pd.isnull(df)
 # print(bool_series)
 
+df = df1.append(df2)
+df = df.append(df3)
 
 # method for dropping all rows with null values
-new_df = df4.dropna(axis=0, how="any")
+new_df = df.dropna(axis=0, how="any")
 
 # #drop duplicates with distinct subset
-# new_df.sort_values("id", inplace = True)
 # new_df.drop_duplicates(subset="id", keep=False, inplace=True)
-# new_df.to_csv('train.csv',index = False,header = True)
-# print(new_df)
 
-# drop duplicate rows
+# drop all duplicate rows
+
 new_df.sort_values("id", inplace=True)
 new_df.drop_duplicates(keep=False, inplace=True)
 new_df.to_csv('train.csv', index=False, header=True)
